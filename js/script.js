73356043aa662b72de824d1a8347e5be99035eac
matteodragotto <matteodrag8@gmail.com>
2024-11-13 15:16:15 +0100

    [feat] console logs comments for discounts

--- js/script.js
+++ js/script.js
@@ -10,7 +10,6 @@ const kmDaPercorrere = parseInt(prompt("Quanti kilometri vuoi percorrere?"));
 const eta = parseInt(prompt("Qual è la tua età?"));
 
 let prezzoPerKm = 0.21;
-const euro = '€';
 const prezzoTotale = prezzoPerKm * kmDaPercorrere
 const scontoUnder18 = 20
 const scontoOver65 = 40
@@ -19,9 +18,9 @@ console.log('Km da percorrere:', kmDaPercorrere);
 console.log('Età del Passeggero:', eta);
 
 if (eta < 18) {
-  console.log ("Prezzo Totale: €", prezzoTotale - (prezzoTotale * scontoUnder18 / 100))
+  console.log ("Prezzo Totale con sconto under18: €", prezzoTotale - (prezzoTotale * scontoUnder18 / 100))
 } else if (eta > 65) {
-  console.log("Prezzo Totale: €", prezzoTotale - (prezzoTotale * scontoOver65 / 100 ))
+  console.log("Prezzo Totale con sconto over65: €", prezzoTotale - (prezzoTotale * scontoOver65 / 100 ))
 }
 else {
   console.log("Prezzo Totale: €", prezzoTotale)
